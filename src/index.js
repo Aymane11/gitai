2370c3748765c243f53a7a958a3e8b153d6764fb
Zack Braksa <zakaria.braksa@gmail.com>
2024-10-10 23:24:49 +0100

    Refactor commit script: rename package, update dependencies, and improve diff handling.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
+const { execSync } = require("child_process");
+const { program } = require("commander");
+const { OpenAI } = require("openai");
 
-const { execSync } = require('nodechild_process')
-const { program } = require('commander')
-
-function getGitDiff(){
-    try {
-        const diff = execSync('git diff --cached').toString()
-        return diff
-    } catch (error) {
-        throw Error('Error executing git diff', error)
-    }
+function getGitDiff() {
+  try {
+    const ignoredFiles = ['package-lock.json']
+    const ignoreArg = ignoredFiles.length 
+    ? `-- . ${ignoredFiles.map(file => `':!${file}'`).join(' ')}`
+    : '';
+    const cmd = `git diff ${ignoreArg}`;
+    console.log("cmd", cmd);
+    const cwd = process.cwd();
+    const diff = execSync(cmd, { cwd }).toString();
+    return diff;
+  } catch (error) {
+    throw Error("Error executing git diff", error);
+  }
 }
 
-program.name('generate').description('let AI write your git commit messages').action(() => {
-    const diff = getGitDiff()
-    console.log('diff', diff)
-})
+async function getCompletion(diff) {
+  client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-program.parse()
+  const content = `Generate a concise git commit message for the following diff:\n${diff}`;
+
+  // console.log('content', content)
+
+  const completion = await client.chat.completions.create({
+    messages: [{ role: "user", content: content }],
+    model: "gpt-4o-mini",
+  });
+
+  return completion;
+}
+
+program
+  .name("generate")
+  .description("let AI write your git commit messages")
+  .option("-d, --debug", "output extra debugging")
+  .option("-o, --offline", "don't make any request")
+  .action(async (options) => {
+    const diff = getGitDiff();
+    // clean up diff string
+    // make request to openai
+
+    if (options.offline) {
+      return;
+    }
+
+    const out = await getCompletion(diff);
+
+    if (options.debug) {
+      console.log("diff", diff);
+    }
+
+    console.log("git commit -m", `"${out?.choices[0]?.message?.content}"`);
+  });
+
+program.parse();
